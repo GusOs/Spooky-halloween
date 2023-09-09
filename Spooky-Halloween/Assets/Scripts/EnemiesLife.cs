1970f6693a914cae4b9ab624bbe56ce7d14c871f
GusOs <gus_92@hotmail.es>
2023-09-09 12:26:48 +0200

    Add audio manager, enemies life and game manager

--- Spooky-Halloween/Assets/Scripts/EnemiesLife.cs
+++ Spooky-Halloween/Assets/Scripts/EnemiesLife.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class EnemiesLife : MonoBehaviour
 {
+    // Vida del enemigo
+    public int maxHealth = 25;
+
+   // Vida actual
+    public int currentHealth;
+
+    // Referencia al slider
+    public Slider slider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        slider.value = currentHealth;
     }
 }
--- Spooky-Halloween/Assets/Scripts/GameManager.cs
+++ Spooky-Halloween/Assets/Scripts/GameManager.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Gamemanager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Instancia de la clase
+    public static GameManager Instance;
+
+    //Variable para comprobar que el juego está activo
+    public bool isGameActive;
+
+    //Objeto del panel wingame
+    public GameObject panelWinGame;
+
+    //Objeto panel rondas
+
+    //Referencia a la vida de las calabazas
+    private PumpkinLife pumpKinScript;
+
+
+    private void Awake()
     {
-        
+        Instance = this;
+        isGameActive = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PumpkinLife = currentHealth.GetComponent<PumpkinLife>();
+        Time.timeScale = 1;
+    }
+
+    public void GameOver() 
+    {
+        if(PumpkinLife.currentHealth <= 0 && isGameActive) 
+        {
+            isGameActive = false;
+
+        }
+    }
+
+    public void Wingame() 
+    {
+        if(isGameActive) 
+        {
+            isGameActive = false;
+            
+            Cursor.lockState = CursorLockMode.Confined;
+        }
     }
 }
